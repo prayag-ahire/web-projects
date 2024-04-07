e1fb424290c4d5e3d4cc097b0a8d5a920a87786f
flash <prayagahire365@gmail.com>
2024-04-07 23:23:57 +0530

    This is a new project call guess-numbers

---
+++ guess-number/scripts/main.js
@@ -0,0 +1,7 @@
+let mybutton = document.querySelector("button");
+mybutton.onclick = ()=>{
+    const title = document.querySelector("h1");
+    title.textContent = `This is me like it`;
+}
+document.addEventListener("DOMContentLoaded",()=>{
+})
